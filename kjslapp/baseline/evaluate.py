ecf78ff7bf9b8b6c0476e17ddd8ae93385e3687a
zhongxiang_sun <51322811+Jeryi-Sun@users.noreply.github.com>
2022-09-27 12:19:42 +0800

    fix bug

--- kjslapp/baseline/evaluate.py
+++ kjslapp/baseline/evaluate.py
@@ -3,8 +3,12 @@ import pandas as pd
 import sys
 from sklearn.metrics import f1_score
 import os
+
 e_min = 0.0000001
+
+
 def rationale_acc(selected_id_a, Case_a_rational, selected_id_b, Case_b_rational, pre_relation, relations):
+
     right_a = 0
     for i in selected_id_a:
         for j in Case_a_rational:
@@ -13,17 +17,17 @@ def rationale_acc(selected_id_a, Case_a_rational, selected_id_b, Case_b_rational
     if len(selected_id_a) == 0:
         P_a = 0
     else:
-        P_a = right_a/len(selected_id_a)
-    
-    if len(Case_a_rational)==0:
+        P_a = right_a / len(selected_id_a)
+
+    if len(Case_a_rational) == 0:
         R_a = 0
     else:
-        R_a = right_a/len(Case_a_rational)
+        R_a = right_a / len(Case_a_rational)
 
     if P_a + R_a <= e_min:
         F_a = 0
     else:
-        F_a = 2*P_a*R_a/(P_a+R_a)
+        F_a = 2 * P_a * R_a / (P_a + R_a)
 
     right_b = 0
     for i in selected_id_b:
@@ -33,17 +37,17 @@ def rationale_acc(selected_id_a, Case_a_rational, selected_id_b, Case_b_rational
     if len(selected_id_b) == 0:
         P_b = 0
     else:
-        P_b = right_b/len(selected_id_b)
-    
-    if len(Case_b_rational)==0:
+        P_b = right_b / len(selected_id_b)
+
+    if len(Case_b_rational) == 0:
         R_b = 0
     else:
-        R_b = right_b/len(Case_b_rational)
+        R_b = right_b / len(Case_b_rational)
 
-    if P_b+R_b <= e_min:
+    if P_b + R_b <= e_min:
         F_b = 0
     else:
-        F_b = 2*P_b*R_b/(P_b+R_b)
+        F_b = 2 * P_b * R_b / (P_b + R_b)
 
     right_c = 0
     for i in pre_relation:
@@ -62,11 +66,12 @@ def rationale_acc(selected_id_a, Case_a_rational, selected_id_b, Case_b_rational
         F_c = 0
     else:
         F_c = 2 * P_c * R_c / (P_c + R_c)
-    return (F_a + F_b + F_c)/3
+    return (F_a + F_b + F_c) / 3
 
 
-base_dir, summit_file = sys.argv[1], sys.argv[2]
-gold_file = os.path.join(base_dir, "competition_stage_1_test_truth.txt") # competition_stage_2_test_truth.txt competition_stage_3_test_truth.txt
+base_dir, summit_file, stage_tag = sys.argv[1], sys.argv[2], sys.argv[3]
+gold_file = os.path.join(base_dir,
+                         f"competition_stage_{stage_tag}_test_truth.txt")  # competition_stage_2_test_truth.txt competition_stage_3_test_truth.txt
 
 gold_data = pd.read_csv(gold_file, sep='\t')
 pred_data = pd.read_csv(summit_file, sep='\t')
@@ -84,11 +89,25 @@ for i in range(len(gold_data)):
     selected_id_b = eval(pred_data.loc[id]['Case_B_rationales'])
     pre_relation = eval(pred_data.loc[id]['relation'])
     pred_labels.append(pred_data.loc[id]['label'])
-    relations_score = rationale_acc(selected_id_a, Case_a_rational, selected_id_b, Case_b_rational, pre_relation, relations)
+
+
+    """去重"""
+    selected_id_a = list(set(selected_id_a))
+    selected_id_b = list(set(selected_id_b))
+    pre_relation_str = []
+    for item in pre_relation:
+        pre_relation_str.append(str(item))
+
+    pre_relation = []
+    for item in list(set(pre_relation_str)):
+        pre_relation.append(eval(item))
+
+
+    relations_score = rationale_acc(selected_id_a, Case_a_rational, selected_id_b, Case_b_rational, pre_relation,
+                                    relations)
     relations_list.append(relations_score)
 f1_macro = f1_score(truth_labels, pred_labels, average='macro')
-print(0.5*f1_macro+0.5*(sum(relations_list)/len(relations_list)))
-
+print(0.5 * f1_macro + 0.5 * (sum(relations_list) / len(relations_list)))
 
 
 
